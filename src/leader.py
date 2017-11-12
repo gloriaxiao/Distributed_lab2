38fcdb0bf81736699db31a15d8e49f35d3ac402d
gloriaxiao <yx259@cornell.edu>
2017-11-12 03:35:13 -0500

    leader init

--- src/leader.py
+++ src/leader.py
@@ -7,6 +7,35 @@ from threading import Lock, Thread
 LEADER_BASEPORT = 25000
 SLEEP = 0.05
 ADDR = 'localhost'
+listener_to_replicas = {}
+sender_to_replicas = {}
+listener_to_acceptors = {}
+sender_to_acceptors = {}
+
+
+def init_leader(lid, num_servers):
+	global listener_to_replicas, sender_to_replicas, listener_to_acceptors, sender_to_acceptors 
+	for i in range(num_servers):
+		listener_to_replicas[i] = LeaderListenerToReplica(lid, i, num_servers)
+		listener_to_replicas[i].start()
+	for i in range(num_leaders):
+		sender_to_replicas[i] = LeaderSenderToReplica(lid, i, num_servers)
+		sender_to_replicas[i].start()
+	for i in range(num_servers):
+		listener_to_acceptors[i] = LeaderListenerToAcceptor(lid, i, num_servers)
+		listener_to_acceptors[i].start()
+	for i in range(num_leaders):
+		sender_to_acceptors[i] = LeaderSenderToAcceptor(lid, i, num_servers)
+		sender_to_acceptors[i].start()
+
+
+class Commander:
+	def __init__(self):
+		pass
+
+class Scout:
+	def __init__(self):
+		pass
 
 
 class LeaderListenerToReplica(Thread): 
